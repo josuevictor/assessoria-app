ef30e918844b4944223a13a77888060ec30ba122
Josue Victor <victorarroxellas01@gmail.com>
2025-10-18 10:26:19 -0300

    mais um ajuste na rota de login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
     setMessage("");
 
     try {
-      const response = await fetch("https://assessoria-api.onrender.com/api", {
+      const response = await fetch("https://assessoria-api.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
